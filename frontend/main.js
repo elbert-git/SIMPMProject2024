bd2ad77939ada57596047645f6043dc979c0e1d1
Elbert Nathanael <elbertwithane@gmail.com>
2024-11-12 22:16:42 +0800

    finished admin flow

--- frontend/main.js
+++ frontend/main.js
@@ -1,40 +1,42 @@
 class Router {
-  constructor(parentElement) {
-    this.root = parentElement;
-    this.goHome();
-  }
-  async go(route) {
-    // clear router
-    this.root.innerHTML = "";
-    //prevent un-authenticated routing
-    await window.render.fromURL(route, this.root);
-  }
-  async goHome() {
-    // based on auth state route to correct page
-    if (window.auth.state.credentials) {
-      if (window.auth.state.credentials.isStaff) {
-        this.go("./pages/admin-dashboard.html");
-      } else {
-        this.go("./pages/student-dashboard.html");
-      }
-    } else {
-      this.go("./pages/home.html");
+    constructor(parentElement) {
+        this.root = parentElement;
+        this.goHome();
     }
-  }
+    async go(route) {
+        // clear router
+        this.root.innerHTML = "";
+        // todo prevent un-authenticated routing
+        // render
+        await window.render.fromURL(route, this.root);
+    }
+    async goHome() {
+        // based on auth state route to correct page
+        console.log(auth.state)
+        if (window.auth.state.credentials) {
+            if (window.auth.state.credentials.isStaff) {
+                this.go("./pages/admin-dashboard.html");
+            } else {
+                this.go("./pages/student-dashboard.html");
+            }
+        } else {
+            this.go("./pages/home.html");
+        }
+    }
 }
 
 (async () => {
-  // initilise auth and api
-  const auth = new Auth();
-  window.auth = auth;
-  //initialse router
-  const router = new Router(document.getElementById("routerRoot"));
-  window.router = router;
-  // intialise components
-  // header
-  await window.render.fromURL(
-    "./pages/headers.html",
-    document.getElementById("headerRoot")
-  );
-  // todo initialise drawer
+    // initilise auth and api
+    const auth = new Auth();
+    window.auth = auth;
+    //initialse router
+    const router = new Router(document.getElementById("routerRoot"));
+    window.router = router;
+    // intialise components
+    // header
+    await window.render.fromURL(
+        "./pages/headers.html",
+        document.getElementById("headerRoot")
+    );
+    // todo initialise drawer
 })();
--- frontend/scripts/rendering.js
+++ frontend/scripts/rendering.js
@@ -1,48 +1,52 @@
 async function fromURL(url, parent) {
-  //load html
-  const html = await fetch(url);
-  const text = await html.text();
-  // render non script elements
-  const div = document.createElement("div");
-  div.innerHTML = text;
-  Array.from(div.children).forEach((element) => {
-    if (element.tagName.toLowerCase() !== "script") {
-      parent.appendChild(element);
-    }
-  });
-  // attach scritp elements
-  Array.from(div.getElementsByTagName("script")).forEach((script) => {
-    const newScript = document.createElement("script");
-    if (script.src) {
-      newScript.src = script.src;
-      newScript.defer = true; // Load in order
-    } else {
-      newScript.textContent = script.textContent;
-    }
-    parent.appendChild(newScript);
-  });
+    //load html
+    const html = await fetch(url);
+    const text = await html.text();
+    // render non script elements
+    const div = document.createElement("div");
+    div.innerHTML = text;
+    Array.from(div.children).forEach((element) => {
+        if (element.tagName.toLowerCase() !== "script") {
+            parent.appendChild(element);
+        }
+    });
+    // attach scritp elements
+    Array.from(div.getElementsByTagName("script")).forEach((script) => {
+        const newScript = document.createElement("script");
+        if (script.src) {
+            newScript.src = script.src;
+            newScript.defer = true; // Load in order
+        } else {
+            newScript.textContent = script.textContent;
+        }
+        parent.appendChild(newScript);
+    });
 }
 
-function renderOneRoom(price, roomCapacity, roomName, id) {
-  return `
+function renderOneRoomForAdmin(price, roomCapacity, roomName, id, isActive) {
+    return `
     <div class="details-container">
       <div class="room-image">
         <img src="./assets/meetingRoom.jpg" alt="Room Image" />
       </div>
 
       <div class="booking-info">
-        <h2 id="room-name">${roomName}</h2>
+        <h2 >${roomName}</h2>
 
         <div>
-          <p><strong>Capacity:</strong> <span id="max-count">${roomCapacity}</span></p>
+          <p><strong>Capacity:</strong> <span >${roomCapacity}</span></p>
         </div>
 
         <div class="amount">
           <p>
-            <strong>Rent per Day:</strong> $<span id="rent-per-day">${price}</span>
+            <strong>Rent per Day:</strong> $<span >${price}</span>
           </p>
         </div>
-
+        <div class="amount">
+          <p>
+            <strong>Available for booking:</strong> <span >${isActive ? "<span style='color:green'>yes</span>" : "<span style='color:red'>no</span>"}</span>
+          </p>
+        </div>
         <button class="buttons room-edit-button" onclick="onEditButtonClicked('${id}')"> Edit </button>
       </div>
     </div>
@@ -50,9 +54,9 @@ function renderOneRoom(price, roomCapacity, roomName, id) {
 }
 
 async function allRooms(parentElement, onlyActive = false) {
-  try {
-    // clear page and creat style
-    parentElement.innerHTML = `
+    try {
+        // clear page and creat style
+        parentElement.innerHTML = `
             <style>
                 .details-container{
                     display: flex; 
@@ -77,28 +81,28 @@ async function allRooms(parentElement, onlyActive = false) {
                 }
             </style>
         `;
-    // get data
-    const res = await auth.getAllRooms();
-    console.log(res);
-    // render elements
-    res.rooms.forEach((roomData) => {
-      // if (roomData.isActive) {
-      parentElement.innerHTML += renderOneRoom(
-        roomData.pricePerHour,
-        roomData.roomCapacity,
-        roomData.roomName,
-        roomData.roomId
-      );
-      // }
-    });
-  } catch (error) {
-    console.log(error);
-  }
+        // get data
+        const res = await auth.getAllRooms();
+        // render elements
+        res.rooms.forEach((roomData) => {
+            // if (roomData.isActive) {
+            parentElement.innerHTML += renderOneRoomForAdmin(
+                roomData.pricePerHour,
+                roomData.roomCapacity,
+                roomData.roomName,
+                roomData.roomId,
+                roomData.isActive
+            );
+            // }
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const render = {
-  fromURL,
-  allRooms,
+    fromURL,
+    allRooms,
 };
 
 window.render = render;
